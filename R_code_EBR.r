129df759bc3daaad2171e54666caf7512d3bc591
Eleonora2103 <62698166+Eleonora2103@users.noreply.github.com>
2021-10-12 09:14:36 +0200

    Update R_code_EBR.r

--- R_code_EBR.r
+++ R_code_EBR.r
@@ -12,6 +12,11 @@ install.packages("readxl")
 install.packages("dplyr")
 install.packages("stats")
 install.packages("modEvA")
+install.packages("mgcv")
+install.packages("glmmsr")
+install.packages("lme4")
+install.packages('Rcpp')
+update.packages("Rcpp")
 
 # Libraries
 library(rgdal)
@@ -21,6 +26,11 @@ library(readxl)
 library(dplyr)
 library(stats)
 library(modEvA)
+library(mgcv)
+library(glmmsr)
+library(lme4)
+library(Rcpp)
+
 
 ## Importo immagine Europe_habitat
 habitat <- readOGR("europe_habitats.shp")
@@ -102,143 +112,60 @@ hr_bioreg_dist.df <- as.data.frame(hr_bioreg_dist)
 summary(hr_bioreg_dist.df)
 str(hr_bioreg_dist.df)
 
+#Conversione da metri a kilometri 
+hr_bioreg_dist.df$min_dst <- hr_bioreg_dist.df$min_dst/1000
+
+##########################################
+for(i in 1:49600){
+  hr_bioreg_dist.df$min_dst[i] <- (hr_bioreg_dist.df$min_dst[i]/1000) 
+}
+##########################################
+
 plot(hr_bioreg_dist.df$min_dst, hr_bioreg_dist.df$hbtt_rc, xlab = "minimum distance", ylab = "habitat richness",
      main = "Minimum distance/Habitat richness")
 abline(lm,lwd = 3,  col= "red")
 
-
 ## LM: habitat richness ~ distanza minima
-lm <- lm(hbtt_rc ~ min_dst, data = hr_bioreg_dist.df)
-summary(lm)
-
-#Call:
-  lm(formula = hbtt_rc ~ min_dst, data = hr_bioreg_dist.df)
-
-#Residuals:
-  Min      1Q  Median      3Q     Max 
-#-26.068  -8.741  -2.011   5.932 108.932 
-
-#Coefficients:
-  Estimate Std. Error t value Pr(>|t|)    
- (Intercept)  2.707e+01  7.810e-02  346.61   <2e-16 ***
-  min_dst     -5.717e-05  9.574e-07  -59.71   <2e-16 ***
-  ---
-  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-#Residual standard error: 13.27 on 49598 degrees of freedom
-#Multiple R-squared:  0.06706,	Adjusted R-squared:  0.06704 
-#F-statistic:  3565 on 1 and 49598 DF,  p-value: < 2.2e-16
-  
-abline(lm,lwd = 3,  col= "red")
+lm.total <- lm(hbtt_rc ~ (min_dst), data = hr_bioreg_dist.df)
+summary(lm.total)
 
 ## Polynomial regression
 polynomial <- lm(hbtt_rc ~ min_dst + I(min_dst^2), data = hr_bioreg_dist.df)
 summary(polynomial)
 
-#Call:
-  lm(formula = hbtt_rc ~ min_dst + I(min_dst^2), data = hr_bioreg_dist.df)
-
-#Residuals:
-  Min      1Q  Median      3Q     Max 
--28.446  -8.446  -1.494   6.232 106.554 
-
-#Coefficients:
-  Estimate Std. Error t value Pr(>|t|)    
-(Intercept)   2.945e+01  8.946e-02  329.13   <2e-16 ***
-  min_dst      -1.820e-04  2.636e-06  -69.04   <2e-16 ***
-  I(min_dst^2)  6.319e-10  1.248e-11   50.63   <2e-16 ***
-  ---
-  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-#Residual standard error: 12.94 on 49597 degrees of freedom
-#Multiple R-squared:  0.1129,	Adjusted R-squared:  0.1129 
-#F-statistic:  3157 on 2 and 49597 DF,  p-value: < 2.2e-16
-
-lines(smooth.spline(min_dst, polynomial), data = hr_bioreg_dist, col="blue", lwd=3)
-
-# using partial F-test
-anova(lm, polynomial)
-
-#Analysis of Variance Table
-
-#Model 1: hbtt_rc ~ min_dst
-#Model 2: hbtt_rc ~ min_dst + I(min_dst^2)
-#Res.Df     RSS Df Sum of Sq      F    Pr(>F)    
-#1  49598 8732564                                  
-#2  49597 8303359  1    429206 2563.7 < 2.2e-16 ***
-  ---
-  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-# fit a model that includes min_dst^3 as well
-model3 <- lm(hbtt_rc ~ min_dst + I(min_dst^2) + I(min_dst^3), data = hr_bioreg_dist.df)
-summary(model3)
-
-attributes(lm)
-
 plot(hr_bioreg_dist.df$min_dst, hr_bioreg_dist.df$hbtt_rc)
 
-poisson <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = hr_bioreg_dist.df)
-summary(poisson)
+#GLM
+poisson.total <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = hr_bioreg_dist.df)
+summary(poisson.total)
 
-#Call:
-  glm(formula = hbtt_rc ~ min_dst, family = poisson(link = "log"), 
-      data = hr_bioreg_dist.df)
-
-#Deviance Residuals: 
-  Min      1Q  Median      3Q     Max  
--6.790  -1.903  -0.406   1.195  14.794  
-
-#Coefficients:
-  Estimate Std. Error z value Pr(>|z|)    
-(Intercept)  3.309e+00  1.169e-03    2831   <2e-16 ***
-  min_dst     -2.688e-06  1.670e-08    -161   <2e-16 ***
-  ---
-  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-#(Dispersion parameter for poisson family taken to be 1)
-
-#Null deviance: 360060  on 49599  degrees of freedom
-#Residual deviance: 331790  on 49598  degrees of freedom
-#AIC: 572923
-
-#Number of Fisher Scoring iterations: 4
-  
-Dsquared(poisson)
+Dsquared(poisson.total)
 
 # [1] 0.07851531
 
-summary(gam(hr_bioreg_dist.df$hbtt_rc ~ s(hr_bioreg_dist.df$min_dst)))
+# GAM
+gam.total <- gam(hbtt_rc ~ s(min_dst, k = 4),  data = hr_bioreg_dist.df, family = poisson())
+summary(gam.total)
+plot(gam.total)
 
-# Family: gaussian 
-# Link function: identity 
+#GLMM
+# habitat_richness ~ distanza_dal_margine + (1|bioreg)
+glmm <- glmm(hbtt_rc ~ min_dst + (1|bioreg), data = hr_bioreg_dist.df, family = binomial, method = "Laplace")
 
-# Formula:
-hr_bioreg_dist.df$hbtt_rc ~ s(hr_bioreg_dist.df$min_dst)
+glmm <- glmer
 
-# Parametric coefficients:
-Estimate Std. Error t value Pr(>|t|)    
-# (Intercept) 24.05363    0.05577   431.3   <2e-16 ***
-
-  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-# Approximate significance of smooth terms:
-edf Ref.df    F p-value    
-# s(hr_bioreg_dist.df$min_dst) 8.975      9 1232  <2e-16 ***
----
-  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-
-# R-sq.(adj) =  0.183   Deviance explained = 18.3%
-# GCV = 154.28  Scale est. = 154.25    n = 49600
-
+# LMM
+lmm.total <- lmer(hbtt_rc ~ min_dst + (1|bioreg), data = hr_bioreg_dist.df, family = gaussian)
+lmm.total <- lme4::lmer(hbtt_rc ~ min_dst + (1|bioreg), data = hr_bioreg_dist.df)
 
 ########### 'Alpine'
 
 alpine <- filter(hr_bioreg_dist.df, bioreg == 'Alpine')
+alpine$min_dst <- alpine$min_dst/1000
 
 lm_alpine <- lm(hbtt_rc ~ min_dst, data = alpine)
 summary(lm_alpine)
-
-plot(alpine$hbtt_rc ~ alpine$min_dst)
+lm_alpine$coefficients
 
 # Polynomial regression
 polynomial.alpine <- lm(hbtt_rc ~ min_dst + I(min_dst^2), data = alpine)
@@ -247,11 +174,20 @@ summary(polynomial.alpine)
 # Calcolo GLM Poisson
 poisson.alpine <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = alpine)
 summary(poisson.alpine)
-plot(poisson.alpine)
+
+Dsquared(poisson.alpine)
+
+# GAM
+gam.alpine <- gam(hbtt_rc ~ s(min_dst),  data = alpine), family = poisson())
+summary(gam.alpine)
+
+# GLMM
+glmm_alpine <- glmm(hbtt_rc ~ min_dst + (1|bioreg), data = alpine, family = binomial, method = "Laplace")
 
 ########### 'Atlantic'
 
 atlantic <- filter(hr_bioreg_dist.df, bioreg == 'Atlantic')
+atlantic$min_dst <- atlantic$min_dst/1000
 
 lm_atlantic <- lm(hbtt_rc ~ min_dst, data = atlantic)
 summary(lm_atlantic)
@@ -267,9 +203,16 @@ poisson.atlantic <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data 
 summary(poisson.atlantic)
 plot(poisson.atlantic)
 
+Dsquared(poisson.atlantic)
+
+# GAM
+gam.atlantic <- gam(hbtt_rc ~ s(min_dst),  data = atlantic, family = poisson())
+summary(gam.atlantic)
+
 ########### 'Black Sea'
 
 black_sea <- filter(hr_bioreg_dist.df, bioreg == 'Black Sea')
+black_sea$min_dst <- black_sea$min_dst/1000
 
 lm_black_sea <- lm(hbtt_rc ~ min_dst, data = black_sea)
 summary(lm_black_sea)
@@ -285,9 +228,16 @@ poisson.black_sea <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data
 summary(poisson.black_sea)
 plot(poisson.black_sea)
 
+Dsquared(poisson.black_sea)
+
+# GAM
+gam.black_sea <- gam(hbtt_rc ~ s(min_dst),  data = black_sea, family = poisson())
+summary(gam.black_sea)
+
 ########## 'Boreal'
 
 boreal <- filter(hr_bioreg_dist.df, bioreg == 'Boreal')
+boreal$min_dst <- boreal$min_dst/1000
 
 lm_boreal <- lm(hbtt_rc ~ min_dst, data = boreal)
 summary(lm_boreal)
@@ -303,9 +253,16 @@ poisson.boreal <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = 
 summary(poisson.boreal)
 plot(poisson.boreal)
 
+Dsquared(poisson.boreal)
+
+# GAM
+gam.boreal <- gam(hbtt_rc ~ s(min_dst),  data = boreal, family = poisson())
+summary(gam.boreal)
+
 ##########  'Continental'
 
 continental <- filter(hr_bioreg_dist.df, bioreg == 'Continental')
+continental$min_dst <- continental$min_dst/1000
 
 lm_continental <- lm(hbtt_rc ~ min_dst, data = continental)
 summary(lm_continental)
@@ -318,12 +275,18 @@ summary(polynomial.continental)
 
 # Calcolo GLM Poisson
 poisson.continental <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = continental)
-summary(poisson.continental )
+summary(poisson.continental)
 plot(poisson.continental)
 
-########## Importo 'Macaronesian'
+Dsquared(poisson.continental)
 
+# GAM
+gam.continental <- gam(hbtt_rc ~ s(min_dst),  data = continental, family = poisson())
+summary(gam.continental)
+
+########## Importo 'Macaronesian'
 macaronesian <- filter(hr_bioreg_dist.df, bioreg == 'Macaronesian')
+macaronesian$min_dst <- macaronesian$min_dst/1000
 
 lm_macaronesian <- lm(hbtt_rc ~ min_dst, data = macaronesian)
 summary(lm_macaronesian)
@@ -339,9 +302,16 @@ poisson.macaronesian <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), d
 summary(poisson.macaronesian)
 plot(poisson.macaronesian)
 
+Dsquared(poisson.macaronesian)
+
+# GAM
+gam.macaronesian <- gam(hbtt_rc ~ s(min_dst),  data = macaronesian, family = poisson())
+summary(gam.macaronesian)
+
 ########## Importo 'Mediterranean'
 
 mediterranean <- filter(hr_bioreg_dist.df, bioreg == 'Mediterranean')
+mediterranean$min_dst <- mediterranean$min_dst/1000
 
 lm_mediterranean <- lm(hbtt_rc ~ min_dst, data = mediterranean)
 summary(lm_mediterranean)
@@ -355,15 +325,20 @@ summary(polynomial.mediterranean)
 # Calcolo GLM Poisson
 poisson.mediterranean <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = mediterranean)
 summary(poisson.mediterranean)
-plot(poisson.mediterranean)
+
+Dsquared(poisson.mediterranean)
+
+# GAM
+gam.mediterranean <- gam(hbtt_rc ~ s(min_dst),  data = mediterranean, family = poisson())
+summary(gam.mediterranean)
 
 ########### 'Pannonian'
 
 pannonian <- filter(hr_bioreg_dist.df, bioreg == 'Pannonian')
+pannonian$min_dst <- pannonian$min_dst/1000
 
 lm_pannonian <- lm(hbtt_rc ~ min_dst, data = pannonian)
 summary(lm_pannonian)
-plot(pannonian$hbtt_rc ~ pannonian$min_dst)
 
 # Polynomial regression
 polynomial.pannonian <- lm(hbtt_rc ~ min_dst + I(min_dst^2), data = pannonia)
@@ -372,29 +347,23 @@ summary(polynomial.pannonian)
 # Calcolo GLM Poisson
 poisson.pannonian <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = pannonian)
 summary(poisson.pannonian)
-plot(poisson.pannonian)
+
+Dsquared(poisson.pannonian)
+
+# GAM
+gam.pannonian <- gam(hbtt_rc ~ s(min_dst),  data = pannonian, family = poisson())
+summary(gam.pannonian)
 
 ########### Steppic
 
 steppic <- filter(hr_bioreg_dist.df, bioreg == 'Steppic')
+steppic$min_dst <- steppic$min_dst/1000
 
 lm_steppic <- lm(hbtt_rc ~ min_dst, data = steppic)
 summary(lm_steppic)
 plot(lm_steppic)
 
 plot(steppic$hbtt_rc, steppic$min_dst)
-
-mean(steppic$hbtt_rc)
-# [1] 6.803828
-
-mean(steppic$min_dst)
-# [1] 25185.55
-
-var(steppic$hbtt_rc)
-#  24.13888
-
-var(steppic$min_dst)
-# [1] 467094878
 
 # Polynomial regression
 polynomial.steppic <- lm(hbtt_rc ~ min_dst + I(min_dst^2), data = steppic)
@@ -403,6 +372,27 @@ summary(polynomial.steppic)
 # Calcolo GLM Poisson
 poisson.steppic <- glm(hbtt_rc ~ min_dst, family = poisson(link = "log"), data = steppic)
 summary(poisson.steppic)
-plot(poisson.steppic)
+
+Dsquared(poisson.steppic)
+
+# GAM
+gam.steppic <- gam(hbtt_rc ~ s(min_dst),  data = steppic, family = poisson())
+summary(gam.steppic)
+
+###############################################################
+
+# Regione Biogeografica come variabile qualitativa
+
+# LM
+lm_bioreg <- lm(hbtt_rc ~ factor(bioreg) + min_dst, data = hr_bioreg_dist.df)
+summary(lm_bioreg)
+
+lm_bioreg$coefficients
+
+# GLM
+glm_bioreg <- glm(hbtt_rc ~ factor(bioreg) + min_dst, data = hr_bioreg_dist.df, family = poisson())
+summary(glm_bioreg)
+
+Dsquared(glm_bioreg)
 
 
